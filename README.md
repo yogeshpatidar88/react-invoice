5d507c3b9d54c9405655d0436b096aa36fbab322
pratik-prajn <pratikprajn007@gmail.com>
2024-12-28 22:00:02 +0530

    nn

---
+++ README.md
@@ -0,0 +1,31 @@
+# React Invoice Generator
+
+React Invoice Generator allows you quickly make invoices and save them as PDF. [https://tuanpham-dev.github.io/react-invoice-generator/](https://tuanpham-dev.github.io/react-invoice-generator/)
+
+![react-invoice-generator](https://raw.githubusercontent.com/tuanpham-dev/react-invoice-generator/master/screenshot.png)
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `yarn start`
+
+Runs the app in the development mode.<br />
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.<br />
+You will also see any lint errors in the console.
+
+### `yarn lint` (`lint:write`)
+
+Launches Prettier, an opinionated code formatter.
+
+https://prettier.io/
+
+### `yarn build`
+
+Builds the app for production to the `build` folder.<br />
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.<br />
+Your app is ready to be deployed!
---
+++ src/data/types.ts
@@ -0,0 +1,55 @@
+import { CSSProperties } from 'react'
+import { z, TypeOf } from 'zod'
+
+export interface ProductLine {
+  description: string
+  quantity: string
+  rate: string
+}
+
+export const TProductLine = z.object({
+  description: z.string(),
+  quantity: z.string(),
+  rate: z.string(),
+})
+
+export const TInvoice = z.object({
+  logo: z.string(),
+  logoWidth: z.number(),
+  title: z.string(),
+  companyName: z.string(),
+  name: z.string(),
+  companyAddress: z.string(),
+  companyAddress2: z.string(),
+  companyCountry: z.string(),
+  billTo: z.string(),
+  clientName: z.string(),
+  clientAddress: z.string(),
+  clientAddress2: z.string(),
+  clientCountry: z.string(),
+  invoiceTitleLabel: z.string(),
+  invoiceTitle: z.string(),
+  invoiceDateLabel: z.string(),
+  invoiceDate: z.string(),
+  invoiceDueDateLabel: z.string(),
+  invoiceDueDate: z.string(),
+  productLineDescription: z.string(),
+  productLineQuantity: z.string(),
+  productLineQuantityRate: z.string(),
+  productLineQuantityAmount: z.string(),
+  productLines: z.array(TProductLine),
+  subTotalLabel: z.string(),
+  taxLabel: z.string(),
+  totalLabel: z.string(),
+  currency: z.string(),
+  notesLabel: z.string(),
+  notes: z.string(),
+  termLabel: z.string(),
+  term: z.string(),
+})
+
+export type Invoice = TypeOf<typeof TInvoice>
+
+export interface CSSClasses {
+  [key: string]: CSSProperties
+}
